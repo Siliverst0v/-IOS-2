3a9d6294784bcc25394859d32cb077baefa62ebe
Siliverst0v <94259002+Siliverst0v@users.noreply.github.com>
2022-10-13 22:28:47 +0300

    Initial Commit

---
+++ Тестовое IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Тестовое IOS
+//
+//  Created by Анатолий Силиверстов on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
